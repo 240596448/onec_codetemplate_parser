76e00877e89d861a923beeb48469dc882374b10e
Vladimir Nadulich <vladimir@nadulich.ru>
2025-10-31 22:53:12 +0300

    test cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,32 +10,32 @@ class Test_CLI:
         result = runner.invoke(app, ["--help"])
         
         assert result.exit_code == 0
-        assert "parse " in result.stdout
-        assert "render " in result.stdout
+        assert "parse " in result.stdout, result.stdout
+        assert "render " in result.stdout, result.stdout
 
     def test_parse_help_command(self):
         """Тест вывода справки команды парсинга"""
         result = runner.invoke(app, ["parse", "--help"])
         
         assert result.exit_code == 0
-        assert "path " in result.stdout
-        assert "src " in result.stdout
+        assert "path " in result.stdout, result.stdout
+        assert "src " in result.stdout, result.stdout
 
     def test_render_help_command(self):
         """Тест вывода справки команды сборки"""
         result = runner.invoke(app, ["render", "--help"])
         
         assert result.exit_code == 0
-        assert "path " in result.stdout
-        assert "src " in result.stdout
+        assert "path " in result.stdout, result.stdout
+        assert "src " in result.stdout, result.stdout
 
     def test_parse_command(self, test_file_path, temp_src):
         """Тест выполнения команды парсинга"""
         result = runner.invoke(app, ["parse", str(test_file_path), str(temp_src)])
-        assert result.exit_code == 0
+        assert result.exit_code == 0, result.stdout + result.stderr
 
     def test_render_command(self, test_file_path, temp_src):
         """Тест выполнения команды сборки"""
-        result = runner.invoke(app, ["render", str(test_file_path), str(temp_src)])
-        assert result.exit_code == 0
+        result = runner.invoke(app, ["render", str(test_file_path), str(temp_src)], catch_exceptions=False)
+        assert result.exit_code == 0, result.stdout + result.stderr
     
